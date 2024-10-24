4ba72ecea15f401f033dbe2d4441fd8e3073081e
IntuneCSAAirlift24Demo <rrifonas@yahoo.com.br>
2024-10-24 12:33:22 -0500

    Create Ingest-IntuneDevices.ps1

---
+++ LogAnalytics/Ingest-IntuneDevices.ps1
@@ -0,0 +1,73 @@
+# Data Collection Rule (DCR) and Data Collection Endpoint (DCE) details
+$DcrImmutableId = "dcr-a658c2efcc4249beaab715b8df62d51b" # DCR ID
+$DceURI = "https://intunedatacdrendpoint-dmwp.eastus-1.ingest.monitor.azure.com" # DCE endpoint where data will be sent
+$Table = "IntuneDevices3_CL" # custom log that must be created already
+
+# Microsoft Entra ID Authentication details
+$tenantId = "" #Azure AD Tenant ID
+$appId = "" #the app ID created and granted permissions
+$appSecret = "" #the secret created for the app id above
+
+# Function to get the Azure AD token (Bearer token) for Graph API
+$body = @{
+    client_id     = $appId
+    scope         = "https://graph.microsoft.com/.default"
+    client_secret = $appSecret
+    grant_type    = "client_credentials"
+}
+$tokenUri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+$headers = @{"Content-Type" = "application/x-www-form-urlencoded"}
+$bearerToken = (Invoke-RestMethod -Uri $tokenUri -Method Post -Body $body -Headers $headers).access_token
+
+# Graph API request headers with the Bearer token
+$graphHeaders = @{
+    "Authorization" = "Bearer $bearerToken"
+    "Content-Type"  = "application/json"
+}
+
+# Graph API URL for querying Intune managed devices
+$graphUri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
+
+# Initialize an empty array to hold device data
+$IntuneDevices = @()
+
+# Loop to handle paginated responses from Graph API
+do {
+    # Get the data from the current page
+    $response = Invoke-RestMethod -Uri $graphUri -Method Get -Headers $graphHeaders
+    
+    # Append current page of events to the list
+    $IntuneDevices += $response.value
+    
+    # Check if there's another page to query
+    $graphUri = $response.'@odata.nextLink'
+    
+} while ($graphUri) # Continue until there's no more pages
+
+# Prepare device information for logging, converting each device into a custom object
+$devicesList = $intuneDevices | ForEach-Object {
+    [PSCustomObject]@{
+        TimeGenerated      = (Get-Date).ToString("dddd MM/dd/yyyy HH:mm K")
+        id                 = $_.id
+        DeviceName         = $_.deviceName
+        UserPrincipalName  = $_.userPrincipalName
+        ModelFriendlyName  = $_.model
+        DeviceManufacturer = $_.manufacturer
+        OperatingSystem    = $_.operatingSystem
+        ComplianceState    = $_.complianceState
+        LastSyncDateTime   = $_.lastSyncDateTime
+    }
+}
+
+# Get a new token for data ingestion
+$scope = [System.Web.HttpUtility]::UrlEncode("https://monitor.azure.com/.default")
+$body = "client_id=$appId&scope=$scope&client_secret=$appSecret&grant_type=client_credentials";
+$headers = @{"Content-Type" = "application/x-www-form-urlencoded" };
+$uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+$bearerToken = (Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers).access_token
+
+# Prepare and upload data to Azure Monitor (DCE)
+$body = $devicesList | ConvertTo-Json
+$headers = @{"Authorization" = "Bearer $bearerToken"; "Content-Type" = "application/json" };
+$uri = "$DceURI/dataCollectionRules/$DcrImmutableId/streams/Custom-$Table"+"?api-version=2023-01-01";
+$uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers;
